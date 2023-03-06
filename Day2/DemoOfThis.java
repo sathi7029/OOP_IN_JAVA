4c3b625f08eb4ab48522b8c44190f5e65c406580
sathi7029 <sathiruidas7029@gmail.com>
2023-03-06 21:11:52 +0530

    day2 uploaded

---
+++ Day2/DemoOfThis.java
@@ -0,0 +1,12 @@
+package Day2;
+public class DemoOfThis {
+    int age;
+    void display(int age){
+        this.age=age;
+        System.out.println(age);
+    }
+    public static void main(String[] args) {
+        DemoOfThis d=new DemoOfThis();
+        d.display(19);
+    }
+}
